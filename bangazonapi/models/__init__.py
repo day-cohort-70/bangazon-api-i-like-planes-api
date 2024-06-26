ce1dd9f42b21935a4e4ef76d57880d48908914a9
William Woodard <williamwoodard98@gmail.com>
2024-06-26 09:53:16 -0500

    removed ratings and added model for store

--- bangazonapi/models/__init__.py
+++ bangazonapi/models/__init__.py
@@ -5,6 +5,5 @@ from .payment import Payment
 from .product import Product
 from .productcategory import ProductCategory
 from .recommendation import Recommendation
-from .rating import Rating
 from .favorite import Favorite
 from .productrating import ProductRating

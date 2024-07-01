fef8abd1683dc928fbe8a79cd7bf6c7144074243
William Woodard <williamwoodard98@gmail.com>
2024-07-01 10:34:03 -0500

    fixed favorites model and fixtures

--- bangazonapi/models/favorite.py
+++ bangazonapi/models/favorite.py
@@ -1,6 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from .customer import Customer
+from .store import Store
 from .productcategory import ProductCategory
 from .orderproduct import OrderProduct
 from safedelete.models import SafeDeleteModel
@@ -9,4 +9,4 @@ from safedelete.models import SOFT_DELETE
 class Favorite(models.Model):
 
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING,)
-    seller = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name='favorited_seller')
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING, related_name='favorited_seller')

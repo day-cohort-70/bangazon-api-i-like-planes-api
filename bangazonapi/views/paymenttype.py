ef8490c0e18cb2742fdc131de5a14680961f6a06
Justin-Acosta <jacostaprof@gmail.com>
2024-07-03 09:49:07 -0500

    corrected create_time to format correctly

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -36,7 +36,7 @@ class Payments(ViewSet):
         new_payment.merchant_name = request.data["merchant_name"]
         new_payment.account_number = request.data["account_number"]
         new_payment.expiration_date = request.data["expiration_date"]
-        new_payment.create_date = datetime.datetime.now()
+        new_payment.create_date = datetime.datetime.now().strftime("%Y-%m-%d")
         customer = Customer.objects.get(user=request.auth.user)
         new_payment.customer = customer
         new_payment.save()

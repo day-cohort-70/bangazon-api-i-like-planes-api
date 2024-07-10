3180e06af73b127a7d10ee166e133ccb002e2781
Justin-Acosta <jacostaprof@gmail.com>
2024-07-10 13:17:25 -0500

    added completed order report

--- bangazonapi/views/reports.py
+++ bangazonapi/views/reports.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from bangazonapi.models import *
+from django.contrib.auth.models import User
 
 
 def ProductsReport(request):
@@ -21,3 +22,25 @@ def StoreReport(request):
 
     }
     return render(request,'allstores.html', context)
+
+def CompletedOrders(request):
+
+    completed_orders = Order.objects.filter(payment_type_id__isnull=False)
+
+    completed_orders_for_html = []
+
+    for order in completed_orders:
+
+        user = User.objects.get(id=order.customer.user_id)
+
+        completed_order_temp = {}
+        completed_order_temp["order_id"] = order.id
+        completed_order_temp["customer_name"] = user.first_name
+        completed_order_temp["total_paid"] = order.total_price
+        completed_order_temp["payment_type"] = order.payment_type
+        completed_orders_for_html.append(completed_order_temp)
+
+
+    context={'completed_orders': completed_orders_for_html}
+
+    return render(request,'completedorders.html',context)

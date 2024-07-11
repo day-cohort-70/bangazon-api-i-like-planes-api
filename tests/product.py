2222f04c4747146908135f2add2d3c5a4b0bfdf7
William Woodard <williamwoodard98@gmail.com>
2024-07-11 13:36:40 -0500

    created a delete test all current tests in this module is functional

--- tests/product.py
+++ tests/product.py
@@ -2,7 +2,7 @@ import json
 import datetime
 from rest_framework import status
 from rest_framework.test import APITestCase
-from bangazonapi.models import Store, Customer
+from bangazonapi.models import Store, Customer, Product
 
 
 class ProductTests(APITestCase):
@@ -105,10 +105,19 @@ class ProductTests(APITestCase):
 
     def test_delete_product(self):
         #create a product to delete
+        self.test_create_product()
+
         #delete that product Ex: response = self.client.delete(f"/games/{game.id}")
+        url = "/products/1"
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(url)
+        
         #verify the status code Ex: self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        #try to GET the product to verify it does NOT exist Ex: response = self.client.get(f"/games/{game.id}")
-        #                                                       self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        #try to GET the product to verify it does NOT exist 
+        #Ex: response = self.client.get(f"/games/{game.id}") / self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
     # TODO: Product can be rated. Assert average rating exists.
